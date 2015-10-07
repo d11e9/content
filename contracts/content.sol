87bfbb9096e448dac9afcb205b05868582943a20
Doug A. <d11e9@users.noreply.github.com>
2015-10-07 14:58:57 +0100

    tip goes to OP or creator

--- contracts/content.sol
+++ contracts/content.sol
@@ -1,4 +1,7 @@
-contract Archive {
+
+import "owned";
+
+contract Archive is owned {
     
     struct Content {
         bool exists;
@@ -6,21 +9,42 @@ contract Archive {
         bytes32[] archives;
     }
     
-    uint totalOC;
-    mapping(uint => bytes32) public OC;
+    uint public price;
     mapping(bytes32 => Content) public contents;
     
-    function archive( bytes32 rootContentHash, bytes32 archive, address OP ) {
+    event Post(bytes32 rootHash, bytes32 contentHash);
+    
+    modifier costs { if (msg.value >= price) _ }
+    
+    function archive( bytes32 rootContentHash, bytes32 archive, address OP, bool tip ) costs {
         address archiver = msg.sender;
         if (contents[rootContentHash].exists && archive != bytes32(0)) {
+            
             // append to archive
             contents[rootContentHash].archives.length++;
             contents[rootContentHash].archives[contents[rootContentHash].archives.length] = archive;
+            Post( rootContentHash, archive);
+            handleTip( tip, OP );
+            
         } else if (!contents[rootContentHash].exists) {
+            
             // first archival
             bytes32[] memory tmp;
             contents[rootContentHash] = Content( true, OP, tmp);
-            OC[totalOC++] = rootContentHash;
+            Post( rootContentHash, bytes32(0));
+            handleTip( tip, OP );
         }
+    }
+    
+    function handleTip (bool tip, address OP) internal {
+        if (tip && OP != address(0)) {
+            OP.send( price );
+        } else {
+            owner.send( price );
+        }
+    }
+    
+    function changePrice(uint _price) onlyowner {
+        price = _price;
     }
 }
